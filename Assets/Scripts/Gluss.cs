94072289e8b0416bf8496d3375d7b7fc577fdd79
Guillaume Lalire <guillaume.lalire@epita.fr>
2021-02-23 15:54:57 +0100

    Added Enemy that follows the Player

---
+++ Assets/Scripts/Gluss.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Gluss : Enemy
+{
+    public Transform player1;
+    public float chaseRadius;
+    public float attackRadius;
+    public Vector3 homePosition;
+    
+    private int _time = 0;
+    private Vector3 _random;
+    public float initialWalkRadius;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1 = GameObject.FindWithTag("Player").transform;
+        homePosition = transform.position;
+        _random = new Vector3(Random.Range(-initialWalkRadius / 2, initialWalkRadius / 2), Random.Range(-initialWalkRadius / 2, initialWalkRadius / 2), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance(player1);
+    }
+
+    // Vérifie la distance avec la cible "target"
+    // Si la distance est inférieure à "chaseRadius" mais supérieure à "attackRadius", l'ennemi se rapproche du joueur
+    // Sinon, l'ennemi se déplace vers une position aléatoire dans le périmètre "initialWalkRadius" autour de son point d'apparition initial "homePosition"
+    // La position aléatoire est modifiée toutes les 150 utilisations de CheckDistance
+    void CheckDistance(Transform target)
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= chaseRadius)
+        {
+            if (distance > attackRadius)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+        else
+        {
+            if (_time % 150 == 0)
+            {
+                _random = new Vector3(Random.Range(-initialWalkRadius / 2, initialWalkRadius / 2), Random.Range(-initialWalkRadius / 2, initialWalkRadius / 2), 0);
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, homePosition + _random, moveSpeed / 2 * Time.fixedDeltaTime);
+        }
+        _time++;
+    }
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        MovePlayer(horizontalMovement,verticalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticalMovement);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+}
