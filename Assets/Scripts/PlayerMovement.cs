291d239674fe958fffe7570fd2ce31c20627951c
Timothée <timothee.drd@gmail.com>
2021-02-24 22:18:44 +0100

    Important ajout d'animations de tiles/de tiles/de script de mouvement
    script movement du personnage
    premiere version du personnage 1 joueur
    ajout materiel pour fix les lignes noirs quand jeu lancé
    ajout palettes beach et random tiles
    ajout tiles animé du bord de mer avec sable et herbe
    ajout du pack beach tiles
    modif du gitignore pour empecher le push de fichier utilisateur de rider
    debut du level design de la plage

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+     
+        }
+    }
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + change.normalized * speed * Time.fixedDeltaTime);
+    }
+}
