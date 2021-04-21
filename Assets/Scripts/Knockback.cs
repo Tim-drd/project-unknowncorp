345fa32fd0a9cd31d1e55eb6d29ae954665c5999
Timothée <timothee.drd@gmail.com>
2021-04-21 18:23:15 +0200

    Ajout de la hitbox knockback et du script correspondant

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public float thrust;
+    public float knockbacktime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Gluss"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if (enemy != null)
+            {
+                enemy.isKinematic = false;
+                Vector2 difference = enemy.transform.position - transform.position;
+                difference = difference.normalized * thrust;
+                enemy.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(enemy));
+            }
+        }
+    }
+    private IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        if (enemy != null)
+        {
+            yield return new WaitForSeconds(knockbacktime);
+            enemy.velocity = Vector2.zero;
+            enemy.isKinematic = true;
+        }
+    }
+}
