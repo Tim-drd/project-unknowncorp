146f78570f8f562fed534b210195320a723ac103
Pierre <pierre.meiss@epita.fr>
2021-04-21 22:22:31 +0200

    Ajout de tous les scripts pour les dialogues
    -Lorsque que l'on s'approche d'un gameObject (nos pnj) avec les scripts correspondants, à partir d'une certaine distance, en appuyant sur I (provisoirement), possibilité d'engager un dialogue avec le pnj.
    -Les textes seront implémentables directement sur chaque pnj dans leur script "Dialogue Trigger" avec la possibiliré d'ajouter pluseurs "slides" de discussions.
    PS: L'apparition du texte est ameé à changer j'ai d'autres idées d'affichage ; je laisse un pnj test au spawn.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun.UtilityScripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+    public Animator anim;
+    
+    public static DialogueManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Plus de DialogueManager dans la scène");
+        }
+
+        instance = this;
+        sentences = new Queue<string>();
+    }
+
+    public void StartD(Dialogues dialogue)
+    {
+        PlayerMovement.instance.enabled = false;
+        anim.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach (var sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            endDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(Typing(sentence));
+    }
+
+    IEnumerator Typing(string sentence) //juste pour donner de l'effet "dialogue" avec le pnj;
+    {
+        dialogueText.text = "";
+        foreach (var letter in sentence)
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.04f);
+        }
+    }
+
+    void endDialogue()
+    {
+        PlayerMovement.instance.enabled = true;
+        anim.SetBool("IsOpen", false);
+    }
+}
