e028e7b3f183ad39c3bba1f6192487de56cd25a8
Guillaume Lalire <guillaume.lalire@epita.fr>
2021-04-23 16:43:37 +0200

    Ajout des potions + fix de bugs
    - ajout de potions de couleurs différentes (en préfabriqués et provisoirement sur la GameScene)
    - fix du bug qui empêchait le joueur de voir le changement d'arme de son coéquipier
    - fix d'une erreur dans le script PlayerHealth

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -12,23 +13,27 @@ public class PlayerHealth : MonoBehaviour
 
     private void Start()
     {
-        hs = Instantiate(heartSystemPrefab, new Vector3(533, -50, 0), Quaternion.identity);
-        hs.transform.SetParent (GameObject.FindGameObjectWithTag("Canvas").transform, false);
-        hs.DrawHeart(health, maxHearts);
+        if (SceneManager.GetActiveScene().name == "GameScene")
+        {
+            hs = Instantiate(heartSystemPrefab, new Vector3(533, -50, 0), Quaternion.identity);
+            hs.transform.SetParent(GameObject.FindObjectOfType<Canvas>().transform, false);
+            hs.DrawHeart(health, maxHearts);
+        }
     }
 
     public void DamagePlayer(float damage)
     {
         health -= damage;
+        if (health < 0)
+            health = 0;
         hs.DrawHeart(health, maxHearts);
     }
     
     public void HealPlayer(float heal)
     {
-        if (health + heal > maxHearts)
+        health += heal;
+        if (health > maxHearts)
             health = maxHearts;
-        else
-            health += heal;
         hs.DrawHeart(health, maxHearts);
     }
 }
