14dbe1e47d2e24bdb7eff0b45972ccc614620dec
Pierre <pierre.meiss@epita.fr>
2021-06-18 12:37:20 +0200

    Script "deactivateWalls" a jour
    - Devrait redonner les tridents/ enlever des boxcolliders/ terminer des quêtes; selon l'avancement de la save

--- Assets/Scripts/DeactivateWallSpawn.cs
+++ Assets/Scripts/DeactivateWallSpawn.cs
@@ -4,31 +4,65 @@ using UnityEngine;
 
 public class DeactivateWallSpawn : MonoBehaviour
 {
-
-
-
-    public BoxCollider2D wall1;
-    public BoxCollider2D wall2;
-    public BoxCollider2D wall3;
-    public BoxCollider2D wall4;
+    public BoxCollider2D[] walls; //y faut les mettre dans le bonne ordre;
+    public GameObject[] pnjs; //dans le bonne ordre;
     public int checkpoint_number;
 
     // Start is called before the first frame update
     void Start()
     {
         checkpoint_number = photonButtons.photonbutton.level;
-
-
         //permet de désactiver les différents murs en fonction de l'avancement de la partie.
         if (checkpoint_number > 0)
-            wall1.enabled = false;
+        {
+            pnjs[0].GetComponent<Quest>().endQuest();
+            walls[0].enabled = false;
+        }
 
-        if(checkpoint_number > 1)
-            wall2.enabled = false;
-
+        if (checkpoint_number > 1)
+        {
+            pnjs[1].GetComponent<Quest>().endQuest();
+            walls[1].enabled = false;
+            walls[2].enabled = false;
+        }
         if (checkpoint_number > 2)
-            wall3.enabled = false;
+        {
+            pnjs[2].GetComponent<Quest>().endQuest();
+            pnjs[3].SetActive(false);
+            pnjs[4].SetActive(true);
+            walls[3].enabled = false;
+            walls[4].enabled = false;
+        }
+        if (checkpoint_number > 3)
+        {
+            pnjs[4].GetComponent<Quest>().endQuest();
+            walls[5].enabled = false;
+        }
 
+        GameObject[] players = GameObject.FindGameObjectsWithTag("PlayerClone");
+        switch (checkpoint_number) //pour attribuer les tridents suivant la progression de la save;
+        {
+            case 1:
+                foreach (var player in players)
+                {
+                    player.GetComponent<Animator>().SetInteger("weaponIndex", 1);
+                }
+                break;
+            case 2:
+                foreach (var player in players)
+                {
+                    player.GetComponent<Animator>().SetInteger("weaponIndex", 2);
+                }
+                break;
+            case 3:
+                foreach (var player in players)
+                {
+                    player.GetComponent<Animator>().SetInteger("weaponIndex", 3);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
 }
