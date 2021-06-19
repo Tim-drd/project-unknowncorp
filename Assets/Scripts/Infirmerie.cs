d8468257ed43c1e23bd1c5d75c7383a61a366731
Pierre <pierre.meiss@epita.fr>
2021-06-20 01:38:00 +0200

    Ajout de plusieurs pnj pour peupler la ville + ajout des infirmiers qui heal les joueurs

---
+++ Assets/Scripts/Infirmerie.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infirmerie : MonoBehaviour
+{
+    public InteractAction i;
+    public Transform t;
+    private int numberOfPlayers;
+    private Transform player1;
+    private Transform player2;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (i.close_enough && KeyBindingManager.GetKeyDown(KeyAction.interact))
+        {
+            if (numberOfPlayers == 2)
+            {
+                if (Vector3.Distance(player1.position, t.position) < Vector3.Distance(player2.position, t.position)) 
+                { 
+                    player1.GetComponent<PlayerHealth>().HealPlayer(10); 
+                }
+                else 
+                { 
+                    player2.GetComponent<PlayerHealth>().HealPlayer(10); 
+                }
+            }
+            else
+            {
+                player1.GetComponent<PlayerHealth>().HealPlayer(10); 
+            }
+        }
+    }
+    
+    void LateUpdate()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("PlayerClone");
+        numberOfPlayers = players.Length;
+        if (numberOfPlayers > 0)
+        {
+            player1 = players[0].transform;
+            if (numberOfPlayers > 1)
+            {
+                player2 = players[1].transform;
+            }
+        }
+    }
+    
+    
+}
